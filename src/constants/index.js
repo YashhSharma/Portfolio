18433ccb77e42bb76c30744b71822e0f758c01bb
Yash Sharma <110838116+YashhSharma@users.noreply.github.com>
2024-06-24 23:42:10 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,9 +3,9 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I bring a diverse skill set and a passion for building innovative digital solutions. With a strong foundation in both front-end and back-end technologies, I am adept at crafting visually appealing and highly functional web applications.My experties are in JavaScript,ReactJs,NodeJs`;
+export const HERO_CONTENT = `I am a passionate front end developer with a knack for crafting robust and scalable web applications. I bring a diverse skill set and a passion for building innovative digital solutions. With a strong foundation in both front-end technologies, I am adept at crafting visually appealing and highly functional web applications.My experties are in JavaScript,ReactJs,NodeJs`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications.My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient and user-friendly web applications.My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
 
 export const EXPERIENCES = [
   {
